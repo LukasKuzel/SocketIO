4c7c26b8f651b378f7a369fca5a34d2235ef810f
Lukas <67201471+LukasKuzel@users.noreply.github.com>
2021-03-23 15:18:48 +0100

    It has to be like this

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
 const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userName = document.getElementById('users');
+
+//Get username and room from URL
+const { username, room} = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+});
 
 const socket = io();
+
+//Join chatroom
+socket.emit('joinRoom', {username, room});
+
+
+//Get room and users
+socket.on('roomUsers', ({room, users}) => {
+    outputRoomName(room);
+    outputUsers(users);
+});
 
 //Message from server 
 socket.on('message', message => {
     console.log(message);
     outputMessage(message);
+
+    //Scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 //Message submit
@@ -13,19 +34,36 @@ chatForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     // Get message text
-    const msg = e.target.elements.msg.value
+    const msg = e.target.elements.msg.value;
 
     // Emit message to server
     socket.emit('chatMessage', msg);
+
+    //Clear input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
 });
 
 //Output message to DOM
 function outputMessage(message){
     const div = document.createElement('div');
     div.classList.add('message');
-    div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+    div.innerHTML = `<p class="meta">${message.username}<span>${message.time}</span></p>
     <p class="text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
-        repudiandae.
+        ${message.text}
     </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
 }
+
+//Add room name to DOM
+function outputRoomName(room){
+    roomName.innerHTML = room;
+}
+
+//Add users to DOM
+function outputUsers(users){
+    userName.innerHTML = `
+        ${users.map(user => `<li>${user.username}</li>`).join('')} 
+    `; 
+}
+
